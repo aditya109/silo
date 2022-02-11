69df34ccff78f77759abbf60a0629d9b4f3f6708
Aditya Kumar <adikid1996@gmail.com>
2022-02-11 23:16:24 +0530

    adding version tag
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const (
+	siloVersion string = "0.0.1"
+	goVersion 
+)
+
+func main() {
+	fmt.Printf(`
+	_ _       
+	(_) |      
+ ___ _| | ___  
+/ __| | |/ _ \ 
+\__ \ | | (_) |
+|___/_|_|\___/ v%s, built with Go for Gophers %s
+
+`, )
+}
